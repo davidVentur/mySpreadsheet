af20b593c7f73ee57038e757036e3a57d2867e22
David Ventura <davidvillesca70@gmail.com>
2022-02-28 19:40:47 -0600

    icono implementados

--- src/app/firstApp/menu/menu.component.ts
+++ src/app/firstApp/menu/menu.component.ts
@@ -6,18 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+  list = document.querySelectorAll('.list');
 
   flag: boolean = false
+  classList: any;
   constructor() { }
 
   ngOnInit(): void {
-
+    this.activeLink()
   }
 
   menuToggle(){
     this.flag = !this.flag
   }
 
+  activeLink(){
+    
+  }
 
 
 }
